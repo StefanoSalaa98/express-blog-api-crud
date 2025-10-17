22ed351809424fc78b38fc7f63d4c586394ce3e3
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-17 16:59:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// importo il framework express
+const express = require("express");
+
+// creo una istanza di express
+const app = express();
+
+// imposto il numero della porta
+const port = 3000;
+
+// importo il modulo del router per i posts
+const postRouter = require("./routers/RouterPosts.js")
+
+// uso il middleware static di express per rendere disponibile i file statici
+app.use(express.static('public'));
+
+// rotte per i posts
+app.use("/posts", postRouter);
+
+// imposto la rotta di home
+app.get("/", (req, res) => {
+    res.send('<h1> Server del mio blog </h1>')
+})
+
+// metto in ascolto il server sulla porta definita
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ routers/RouterPosts.js
@@ -0,0 +1,56 @@
+// importo il framework express
+const express = require("express");
+
+// attivo il router
+const router = express.Router();
+
+// Rotte di CRUD sulla risorsa posts
+
+// importo la lista dei posts
+const posts = require("../data/posts.js");
+
+// Creo una rotta /index che restituisca un oggetto json con la lista dei post.
+router.get("/index", function (req, res) {
+
+    // invio la risposta con il json relativo
+    res.json(posts);
+})
+
+// Creo una rotta /show/:id che restituisca un singolo post
+router.get("/show/:id", function (req, res) {
+
+    const post = posts[req.params.id];
+    // invio la risposta con il json relativo
+    res.json(post);
+})
+
+// index
+router.get('/', function (req, res) {
+    res.send('Lista dei posts');
+});
+// show
+router.get('/:id', function (req, res) {
+    res.send('Dettagli del singolo post ' + req.params.id);
+});
+// store
+router.post('/', function (req, res) {
+    res.send('Creazione nuovo post');
+});
+
+// update
+router.put('/:id', function (req, res) {
+    res.send('Modifica integrale di uno specifico post ' + req.params.id);
+});
+
+// modify
+router.patch('/:id', function (req, res) {
+    res.send('Modifica parziale di uno specifico post ' + req.params.id);
+});
+
+// destroy
+router.delete('/:id', function (req, res) {
+    res.send('Eliminazione di uno specifico post ' + req.params.id);
+});
+
+
+module.exports = router;
