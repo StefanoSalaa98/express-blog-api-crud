fb370ead12ce9c618d199b4cae174c84418c9037
StefanoSalaa98 <sala.stefano98@gmail.com>
2025-10-17 17:24:18 +0200

    Milestone 1: Creazione postsController

---
+++ controllers/postsController.js
@@ -0,0 +1,84 @@
+// importiamo i dati della risorsa
+const posts = require('../data/posts');
+
+function index(req, res) {
+    // Il posts filtrato di partenza corrisponde a quello originale
+    let filteredposts = posts;
+
+    // Se la richiesta contiene un filtro, allora filtro il posts tramite i metodi filter ed includes
+    if (req.query.ingredient) {
+        filteredposts = posts.filter(
+            post => post.ingredients.includes(req.query.ingredient)
+        );
+    }
+
+    // restituisco la variabile filteredposts che contiene il posts filtrato
+    res.json(filteredposts);
+}
+
+function show(req, res) {
+    // recupero l'id dall' URL e lo converto in un numero per poterlo conforntare con gli altri id
+    const id = parseInt(req.params.id)
+
+    // cerco il post tramite id
+    const post = posts.find(post => post.id === id);
+
+    // Controllo se il post è stato trovato
+    if (!post) {
+
+        //Imposto lo status 404
+        res.status(404)
+
+        // Restituisco un JSON con le altre informazioni (posso scrivere quello che voglio)
+        return res.json({
+            error: "Not Found",
+            message: "post non trovata"
+        })
+    }
+
+    // Se sono arrivato fin qui, signfica che il post corrispondente è stato trovato, lo restituisco quindi sotto forma di JSON   
+    res.json(post);
+}
+
+function store(req, res) {
+    res.send('Creazione nuovo post');
+}
+
+function update(req, res) {
+    res.send('Modifica integrale del post ' + req.params.id);
+}
+
+function modify(req, res) {
+    res.send('Modifica parziale del post ' + req.params.id);
+}
+
+function destroy(req, res) {
+    // recupero l'id dall' URL e lo converto in un numero per poterlo conforntare con gli altri id
+    const id = parseInt(req.params.id)
+
+    // cerco il post tramite id
+    const post = posts.find(post => post.id === id);
+
+    // // Controllo se il post da eliminare è stato trovato
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "post non trovata"
+        })
+    }
+
+    // Rimuovo il post dall'elenco dei posts
+    posts.splice(posts.indexOf(post), 1);
+
+    // stampo l'elenco dei posts in console per controllare che sia stato eliminato correttamente
+    console.log(posts);
+
+    // Restituisco lo status corretto
+    res.sendStatus(204)
+}
+
+// esporto le funzioni che ho creato
+module.exports = { index, show, store, update, modify, destroy }
--- routers/RouterPosts.js
+++ routers/RouterPosts.js
@@ -4,56 +4,26 @@ const express = require("express");
 // attivo il router
 const router = express.Router();
 
-// Rotte di CRUD sulla risorsa posts
+// importo il controller della risorsa posts
+const postsController = require('../controllers/postsController');
 
-// importo la lista dei posts
-const posts = require("../data/posts.js");
+// Rotta /index che restituisca un oggetto json con la lista dei post filtrati o non.
+router.get('/', postsController.index);
 
-// Creo una rotta /index che restituisca un oggetto json con la lista dei post.
-router.get("/index", function (req, res) {
+// Rotta /show/:id che restituisca un singolo post
+router.get('/:id', postsController.show);
 
-    // invio la risposta con il json relativo
-    res.json(posts);
-})
-
-// Creo una rotta /show/:id che restituisca un singolo post
-router.get("/show/:id", function (req, res) {
-
-    // converto l'id ricevuto in un numero intero
-    const id = parseInt(req.params.id)
-    // attraverso il metodo find cerco l'eleemnto corrispondente a quello richiesto confrontando gli id
-    const post = posts.find(post => post.id === id);
-    // invio la risposta con il json relativo
-    res.json(post);
-})
-
-// index
-router.get('/', function (req, res) {
-    res.send('Lista dei posts');
-});
-// show
-router.get('/:id', function (req, res) {
-    res.send('Dettagli del singolo post ' + req.params.id);
-});
 // store
-router.post('/', function (req, res) {
-    res.send('Creazione nuovo post');
-});
+router.post('/', postsController.store);
 
 // update
-router.put('/:id', function (req, res) {
-    res.send('Modifica integrale di uno specifico post ' + req.params.id);
-});
+router.put('/:id', postsController.update);
 
 // modify
-router.patch('/:id', function (req, res) {
-    res.send('Modifica parziale di uno specifico post ' + req.params.id);
-});
+router.patch('/:id', postsController.modify);
 
 // destroy
-router.delete('/:id', function (req, res) {
-    res.send('Eliminazione di uno specifico post ' + req.params.id);
-});
+router.delete('/:id', postsController.destroy);
 
 
 module.exports = router;
